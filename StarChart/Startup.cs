9c385935498f7b33aff46b44592f3cdbf142983a
Pandurangasricharan Yerragudi <M1044318@mindtree.com>
2020-10-28 11:19:17 +0530

    Completed module

--- StarChart/Startup.cs
+++ StarChart/Startup.cs
@@ -13,7 +13,7 @@ namespace StarChart
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("StarChart"))
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("StarChart"));
             
         }
 
