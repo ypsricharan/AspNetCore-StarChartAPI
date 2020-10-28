9bf2cc10cd9ae87201d3bf1b7576f875e7deb9d1
Pandurangasricharan Yerragudi <M1044318@mindtree.com>
2020-10-28 13:19:43 +0530

    Completed module

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -102,7 +102,7 @@ namespace StarChart.Controllers
         }
 
         [HttpDelete("{id}")]
-        public IActionResult RenameObject(int id)
+        public IActionResult Delete(int id)
         {
 
             var existingObjects = _context.CelestialObjects.Where(e => e.Id == id || e.OrbitedObjectId == id);
